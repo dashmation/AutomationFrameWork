76058c2251ef3e442868eab185f87d9a6a507a47
dashmation <44187454+dashmation@users.noreply.github.com>
2020-07-14 02:24:47 +0530

    Comment updated

--- src/main/java/com/qa/amazonapp/SelectADeliveryAddress.java
+++ src/main/java/com/qa/amazonapp/SelectADeliveryAddress.java
@@ -8,6 +8,10 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 
+/*
+ * This Class represents the page Object of SelectADeliveryAddress
+ */
+
 public class SelectADeliveryAddress extends CommonMobileMethods {
 
 	@AndroidFindBy(className = "")
