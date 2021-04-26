7fde42da7d89f493e16145e5b2d18a41186c1f35
subhrajyoti.dash <subhrajyoti.dash@oracle.com>
2021-04-26 23:51:51 +0530

    fix @before suite duplicacy

--- src/main/java/com/qa/basePack/CommonTest.java
+++ src/main/java/com/qa/basePack/CommonTest.java
@@ -3,6 +3,7 @@ package com.qa.basePack;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
@@ -39,7 +40,7 @@ public class CommonTest extends DriverTech{
 		}
 	}
 	
-	@BeforeSuite(alwaysRun = true)
+	@AfterSuite(alwaysRun = true)
 	public void suiteEnd() {
 		System.out.println("#######################################Test Suite Completed####################################");
 	}
